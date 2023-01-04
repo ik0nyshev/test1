394b95d1853fc9885170bae33af0f042cc3527a4
ikonyshev <igor.konyshev@graebert.com>
2023-01-05 00:04:03 +0300

    simple cmake project

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <build.h>
+int main(int argc, char **argv)
+{
+    std::cout <<"build " << BUILD_VERSION <<std::endl; 
+    std::cout <<"Hello World!" <<std::endl; 
+    return 0;
+}
